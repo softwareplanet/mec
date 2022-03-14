aa01d392ad190b0919e5ac9bbd30a712dec7d260
Serhii Vodianov <serhiivo@tahometer.com>
2022-03-14 19:36:11 +0200

    add header to info page

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../templates/InfoPage.css";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import tank from "../equipment/tanks/T-90/images/t-90.png"
 
 export const query = graphql`
         query ($slug: String) {
@@ -17,9 +18,15 @@ export const query = graphql`
 const InfoPage = (props) => {
 
     return (
-        <div className="infoPage">
-            <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
-        </div >
+        <>
+            <div className="header">
+                <h1>❮ Taнки</h1>
+                <img src={tank} /> {/* Slider component */}
+            </div>
+            <div className="infoPage">
+                <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
+            </div >
+        </>
     );
 }
 
