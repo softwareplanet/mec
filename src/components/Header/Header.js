521ffd57bf896537e84caffe9c8da78fa923be26
Inna Kryvych <innakr@tahometer.com>
2022-03-14 08:40:37 +0200

    change svg icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import "./Header.css"
-import React from "react";
+import React, { useState, useEffect } from "react";
 import tank from "../../images/tank_img.png"
 import Switcher from "../Switcher/Switcher";
 
 
 let Header = () => {
+
+    let [change, setChange] = useState(false)
+
+    let clickHandler = (className) => {
+        // if (!change) {
+        //     setChange(!change)
+        //     className = "icon active"
+        //     console.log(className)
+        // }
+        // else {
+            // setChange(!change)
+            // className = "icon"
+            console.log(className)
+        // }
+    }
+
     return (
         <div className="header">
             <div className="head">
@@ -13,7 +29,7 @@ let Header = () => {
             </div>
             <div className="lookup">
                 <input type="search" placeholder="Пошук..." autoComplete="off"/>
-                <Switcher />
+                <Switcher onClick={clickHandler}/>
             </div>
         </div>
     )
