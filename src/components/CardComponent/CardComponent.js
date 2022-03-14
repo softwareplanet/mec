2a6afe822c6e28f479ec550b197f4ce10fd12ed6
Vitalii Hrabovych <98694678+VitaliyHrabovych@users.noreply.github.com>
2022-03-14 15:45:45 +0200

    add Link for CardComponent

--- src/components/CardComponent/CardComponent.js
+++ src/components/CardComponent/CardComponent.js
@@ -2,15 +2,19 @@ import React from 'react'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import "./CardComponent.css"
+import { Link } from 'gatsby'
 
-let CardComponent = ({title, image}) => {
+let CardComponent = ({key, title, image}) => {
     return (
-        <div className="military-equipment">
-            <div className="img-container">
-                <GatsbyImage imgClassName="png-img" image={getImage(image)} alt={title} />
+        <Link to={`/${key}`} className="link-view">
+            <div className="military-equipment">
+                <div className="img-container">
+                    <GatsbyImage imgClassName="png-img" image={getImage(image)} alt={title} />
+                </div>
+                <h2>{title}</h2>
             </div>
-            <h2>{title}</h2>
-        </div>
+        </Link>
+        
   )
 }
 
