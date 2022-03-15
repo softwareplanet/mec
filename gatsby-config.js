8b74edb6fabde31adc35a1e0294b85a270cf190b
Inchyk <kryvych.inna1@gmail.com>
2022-03-15 17:43:49 +0200

    delete precache pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-offline`,
       options: {
-        precachePages: [`/*/*`],
+        // precachePages: [`/*/*`],
         workboxConfig: {
           globPatterns: ['**/src/images/icon.png*']
         },
