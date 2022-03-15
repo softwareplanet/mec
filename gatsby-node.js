3a59d28f8e24fd4ee7207543aea106db46b088b5
Vitalii Hrabovych <98694678+VitaliyHrabovych@users.noreply.github.com>
2022-03-15 14:38:34 +0200

    add new slugs onto the markdownRemark

--- gatsby-node.js
+++ gatsby-node.js
@@ -36,3 +36,15 @@ exports.createPages = async ({ actions: { createPage }, graphql }) => {
         })
     })
 }
+const { createFilePath } = require(`gatsby-source-filesystem`)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
