944c380c4ea69f98260017ff2e6b7bc778c6086f
Serhiy <76167269+G0FiN@users.noreply.github.com>
2022-03-16 12:52:10 +0200

    fix using of graphql query

--- src/pages/{CategoriesYaml.name}.js
+++ src/pages/{CategoriesYaml.name}.js
@@ -5,34 +5,30 @@ import Header from "../components/Header/Header";
 import CategoryList from "../components/RenderList/CategoryList";
 
 export const query = graphql`
-  query ($name: String){
+  query ($name: String) {
     categoriesYaml (name: {eq: $name}) {
       title
-    }
-    allMdx(filter: {frontmatter: {category: {eq: $name}}}) {
-        nodes {
-          frontmatter {
-            category
-            image {
-              childImageSharp {
-                gatsbyImageData (width: 160, height: 160)
-              }
+      equipment {
+        frontmatter {
+          image {
+            childImageSharp {
+              gatsbyImageData (width: 160, height: 160)
             }
-            title
           }
-          slug
+          title
         }
+        slug
+      }
     }
   }
 `
 
 let CategoryPage = ({ data }) => {
   const category = data.categoriesYaml;
-  let [view, setView] = useState('grid')
   return (
     <div className={styles.addMargins}>
       <Header name={category.title} backPath="/"/>
-      <CategoryList data={data.allMdx.nodes}/>
+      <CategoryList data={category.equipment}/>
     </div>
   )
 }
