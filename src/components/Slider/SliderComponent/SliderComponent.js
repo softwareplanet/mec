919b14894f42ec7f9c91c6fd57c363c0e29e5164
Ihor Molchan <ihormo@tahometer.com>
2022-03-16 14:42:35 +0200

    [#5] - add arrows to slide element

--- src/components/Slider/SliderComponent/SliderComponent.js
+++ src/components/Slider/SliderComponent/SliderComponent.js
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import SlideElement from '../SlideElement/SlideElement';
 import gun from '../../../equipment/tanks/t-90/images/pic2.png';
+import arrow from './arrow.png'
 import { graphql } from 'gatsby';
 
 export const query = graphql`
@@ -23,6 +24,27 @@ export const query = graphql`
         }
     `;
 
+
+const NextArrow = (props) => {
+  return (
+    <div className={styles.arrow}>
+      <span className={styles.nextArrow} onClick={props.onClick}>
+        <img src={arrow} alt="nextArrow" />
+      </span>
+    </div>
+  );
+}
+
+const PrevArrow = (props) => {
+  return (
+    <div className={styles.arrow}>
+      <span className={styles.prevArrow} onClick={props.onClick}>
+        <img src={arrow} alt="nextArrow" />
+      </span>
+    </div>
+  );
+}
+
 const SliderComponent = () => {
 
   const images = [gun, gun, gun];
@@ -31,10 +53,12 @@ const SliderComponent = () => {
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
   }
 
   return (
-    <div className={styles.carousel}>
+    <div>
       <Slider {...settings}>
         {images.map((image, index) => (
           <SlideElement slideImage={image} key={index} allImages={images.length} currentImage={index + 1} />
