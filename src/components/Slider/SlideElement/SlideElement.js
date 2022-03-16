45fae3f6f8da885837dfc755fa58048ac16eb48e
Ihor Molchan <ihormo@tahometer.com>
2022-03-16 13:58:24 +0200

    [#5] - add a block with the number offf pictures

--- src/components/Slider/SlideElement/SlideElement.js
+++ src/components/Slider/SlideElement/SlideElement.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import * as styles from './SlideElement.module.css';
+import vector from './vector.png'
 
-export default function SlideElement({ slideImage }) {
+export default function SlideElement({ slideImage, allImages, currentImage }) {
   return (
-    <div>
+    <div className={styles.SlideElement}>
       <img src={slideImage} className={styles.imageSlide} alt='name' />
+      <div className={styles.countImages}>
+        <img src={vector} alt="vector" />
+        <span>{currentImage} з {allImages}</span>
+      </div>
     </div>
   )
 }
