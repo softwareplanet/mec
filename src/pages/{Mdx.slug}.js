b2263dd829418d49357ce40ae552e11e78224e0d
Serhiy <76167269+G0FiN@users.noreply.github.com>
2022-03-16 12:48:01 +0200

    use custom field in graphql query,  add using of universal header,  add slider with hardcoded images, add new button with link for more information

--- src/pages/{Mdx.slug}.js
+++ src/pages/{Mdx.slug}.js
@@ -1,16 +1,21 @@
 import React from "react";
 import * as styles from "../components/InfoPage.module.css";
+import * as header from "../components/index.module.css"
 import { graphql, Link } from "gatsby";
+import Header from "../components/Header/Header";
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-
+import Slider from "../components/Slider/ImageSlider/Slider";
 
 export const query = graphql`
         query ($slug: String) {
             mdx(slug:{eq:$slug}) {
                 frontmatter{
                     title
-                    category
+                    category {
+                        name
+                        title
+                    }
                     image {                        
                         childImageSharp {
                           gatsbyImageData 
@@ -23,16 +28,23 @@ export const query = graphql`
     `;
 
 const InfoPage = (props) => {
+    console.log(props.data);
+
+    const { category } = props.data.mdx.frontmatter;
 
     return (
         <>
-            <div className={styles.header}>
-                <Link to={`/${props.data.mdx.frontmatter.category}`}><h1>❮ titleOfCategory</h1></Link>                
-                <GatsbyImage layout="fullWidth" image={getImage(props.data.mdx.frontmatter.image.childImageSharp)} alt="" /> {/* Slider component */}
+            <div className={header.addMargins}>
+                <Header name={category.title} backPath={`/${category.name}`} />
             </div>
-            <div className={styles.infoPage}
-            >
+            <div className={styles.header}>
+                <Slider />                
+            </div>
+            <div className={styles.infoPage} >
                 <MDXRenderer>{props.data.mdx.body}</MDXRenderer>
+                <div className={styles.links}>
+                    <a target="_blank" href="https://uk.wikipedia.org/">Детальніше</a>
+                </div>
             </div >
         </>
     );
