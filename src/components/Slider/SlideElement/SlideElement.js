3b225cc567780b435c209923dbb195a5eac8141a
Ihor Molchan <ihormo@tahometer.com>
2022-03-21 18:44:28 +0200

    [#5] - refactor slide element

--- src/components/Slider/SlideElement/SlideElement.js
+++ src/components/Slider/SlideElement/SlideElement.js
@@ -6,7 +6,12 @@ import { BiImages } from 'react-icons/bi';
 export default function SlideElement({ slideImage, allImages, currentImage }) {
   return (
     <div className={styles.SlideElement}>
-      <GatsbyImage image={slideImage} alt="Slide image" />
+      <GatsbyImage
+        image={slideImage}
+        // imgStyle={{ objectFit: 'contain', maxWidth: '100%' }}
+        // style={{ height: '50vh' }}
+        alt="Slide image"
+      />
       <div className={styles.countImages}>
         <BiImages className={styles.img} />
         <span>{currentImage} з {allImages}</span>
--- src/templates/equipment-page.js
+++ src/templates/equipment-page.js
@@ -26,7 +26,7 @@ export const query = graphql`
                         quality: 100
                         layout: FULL_WIDTH
                         placeholder: BLURRED
-                        formats: [AUTO, WEBP, PNG]
+                        formats: [AUTO, WEBP, JPG]
                     )
                 }
             }
