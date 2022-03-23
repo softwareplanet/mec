9bef4278b7bd91d3a19236932ff5a4b283f7d96e
Vitalii Hrabovych <98694678+VitaliyHrabovych@users.noreply.github.com>
2022-03-23 14:07:56 +0200

    add static query for search component

--- src/components/ToolBar/Search.js
+++ src/components/ToolBar/Search.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import * as styles from "../RenderList/RenderList.module.css";
 import lookup from "../../images/lookup.svg";
-let Search = () => {
+import {Index} from 'elasticlunr';
+
+
+let Search = ({searchIndex}) => {
+    let [searchState, setSearchState] = useState({
+        query: ``,
+        results: []
+    })
+    console.log(searchState);
+    const getIndex = () => Index.load(searchIndex);
     return (
         <>
             <img className={styles.lookup} src={lookup} alt="" />
-            <input type="search" placeholder="Пошук..." autoComplete="off" />
+            <input type="search" placeholder="Пошук..." autoComplete="off" value={searchState.query} onChange={(evt) => {
+                const query = evt.target.value; 
+                const index = getIndex();
+                    setSearchState({
+                        query,
+                        results: index.search(query).map(({
+                            ref,
+                        }) => index.documentStore.getDoc(ref)),
+                    });
+                }
+            }/>
+            <ul>
+                {
+
+                }
+            </ul>
         </>
     )
 }
