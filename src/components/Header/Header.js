e60ae88bb03eda452d86ad684f3bb299fb9424b9
Serhii Vodianov <serhiivo@tahometer.com>
2022-03-25 13:46:58 +0200

    change return back arrow icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,13 @@ import { Link } from "gatsby";
 import React from "react";
 import tank from "../../images/tank_img.png"
 import * as styles from "./Header.module.css"
-import { IoIosArrowBack } from "react-icons/io";
+import arrow from "../../equipment/images/arrow-left.png";
 
 let Header = (props) => {
     return (
         <Link to={props.backPath || "/"}>
             <div className={styles.head}>
-                {props.backPath ? <IoIosArrowBack /> : <img src={tank} alt="" className={styles.logo} />}
+                {props.backPath ? <img height="24px" src={arrow} /> : <img src={tank} alt="" className={styles.logo} />}
                 <h1>{props.name}</h1>
             </div>
         </Link>
