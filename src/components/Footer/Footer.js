b497e6af23b38f6f82afc4c4361931dbc29fcc7b
Ihor Molchan <ihormo@tahometer.com>
2022-03-31 16:02:41 +0300

    Refactor footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,14 +5,16 @@ export default function Footer({ commitInfo, tagInfo }) {
   const hashCommit = commitInfo.hash.substring(0, 7);
   const tagName = tagInfo.name.substring(1);
   const dateCommit = commitInfo.date;
-  const linkCommit = `https://github.com/softwareplanet/mec/commit/${hashCommit}`
+  const linkCommit = `https://github.com/softwareplanet/mec/commit/${hashCommit}`;
+  const tagLink = `https://github.com/softwareplanet/mec/releases/tag/${tagInfo.name}`
 
   return (
     <div className={styles.versionInfo}>
-      <span>Version&nbsp;</span>
-      <a href={linkCommit} target='_blank' rel="noreferrer">{tagName}+</a>
-      <span>sha.{hashCommit}</span>
-      <span>-{dateCommit}</span>
+      Version&nbsp;
+      <a href={tagLink} target='_blank' rel="noreferrer">{tagName}</a>
+      +
+      <a href={linkCommit} target='_blank' rel="noreferrer">sha.{hashCommit}</a>
+      -{dateCommit}
     </div>
   )
 }
