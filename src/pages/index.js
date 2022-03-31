f3bdc94bf03cf87715cf0e33297b8ccc9ca312e2
Ihor Molchan <ihormo@tahometer.com>
2022-03-31 11:04:06 +0300

    Refactor index file with layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import "../components/reset.css";
-import * as styles from "../components/index.module.css";
 import React from "react";
-import Header from "../components/Header/Header";
+import '../components/reset.css';
+import * as styles from '../components/index.module.css';
 import RenderList from "../components/RenderList/RenderList";
+import Layout from '../components/Layout/Layout';
 import { graphql } from "gatsby";
 
 export const query = graphql`
@@ -29,25 +29,17 @@ export const query = graphql`
 `;
 
 let FirstPage = ({ data }) => {
-  const hashCommit = data.gitCommit.hash.substring(0, 7);
-  const tagName = data.gitTag.name.substring(1);
-  const dateCommit = data.gitCommit.date;
-
   return (
     <div className={styles.addMargins}>
-      <Header name="Військова техніка" />
-      <RenderList
-        data={data.allCategoriesYaml.nodes.map((n) => ({
-          path: n.name,
-          image: n.image.childImageSharp,
-          title: n.title,
-        }))}
-      />
-      <div className={styles.versionInfo}>
-        <span>Version {tagName}</span>
-        <a href={`https://github.com/softwareplanet/mec/commit/${hashCommit}`} target='_blank' rel="noreferrer">+sha.{hashCommit}</a>
-        <span>-{dateCommit}</span>
-      </div>
+      <Layout commitInfo={data.gitCommit} tagInfo={data.gitTag}>
+        <RenderList
+          data={data.allCategoriesYaml.nodes.map((n) => ({
+            path: n.name,
+            image: n.image.childImageSharp,
+            title: n.title,
+          }))}
+        />
+      </Layout>
     </div>
   );
 };
