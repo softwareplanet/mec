5c183149dcc2468c1a33e2eca5bf7708b38d9fbf
Serhii Vodianov <serhiivo@tahometer.com>
2022-04-01 14:04:50 +0300

    add adaptive size for grids

--- src/components/RenderList/RenderList.js
+++ src/components/RenderList/RenderList.js
@@ -2,25 +2,37 @@ import React, { useEffect, useRef, useState } from "react";
 import CardComponent from "../CardComponent/CardComponent";
 import ToolBar from "../ToolBar/ToolBar";
 import * as styles from "./listsStyles.module.css";
-import addEmptySpaces from "./EmptySpaces";
-import { fromEvent, throttleTime } from "rxjs";
+import { debounceTime, fromEvent, startWith } from "rxjs";
+
+const LAYOUT_MARGIN = 20;
+const MAX_CONTAINER_WIDTH = 900;
+const DEFAULT_MOBILE_CONTAINER_WIDTH = 335;
 
 let RenderList = ({ data, searchData }) => {
   let [view, setView] = useState("grid");
 
-  let [containerWidth, setWindowWidth] = useState(
-    typeof window !== `undefined` ? window.innerWidth : 0
+  let [containerWidth, setContainerWidth] = useState(
+    typeof window !== `undefined` ? Math.min(window.innerWidth - LAYOUT_MARGIN * 2, MAX_CONTAINER_WIDTH) : DEFAULT_MOBILE_CONTAINER_WIDTH
   );
   useEffect(() => {
     const subscribtion = fromEvent(window, "resize")
-      .pipe(throttleTime(250))
-      .subscribe(() => {
-        setWindowWidth(container.current.clientWidth);
-      });
+      .pipe(
+        debounceTime(250),
+        startWith(DEFAULT_MOBILE_CONTAINER_WIDTH) // need to path some value
+      )
+      .subscribe(() => setContainerWidth(container.current.clientWidth));
     return () => subscribtion.unsubscribe();
   }, []);
 
   const container = useRef();
+
+  const culcCardSize = () => {
+    let result;
+    let cardNumber = Math.round(containerWidth / 160);
+    result = (containerWidth - (15 * (cardNumber - 1))) / cardNumber;
+    return result;
+  };  
+  const cardSize = culcCardSize();
 
   return (
     <>
@@ -33,11 +45,9 @@ let RenderList = ({ data, searchData }) => {
             image={element.image}
             title={element.title}
             variant={view}
+            size={cardSize}
           />
-        ))}
-        {typeof window !== `undefined`
-          ? addEmptySpaces(containerWidth, data.length)
-          : () => {}}
+        ))}        
       </div>
     </>
   );
