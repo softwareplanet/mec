5b202f1d8c11850cde4608191567cb94a7c75483
Inchyk <kryvych.inna1@gmail.com>
2022-04-01 16:29:20 +0300

    fix first loading render

--- src/components/RenderList/RenderList.js
+++ src/components/RenderList/RenderList.js
@@ -4,21 +4,17 @@ import ToolBar from "../ToolBar/ToolBar";
 import * as styles from "./listsStyles.module.css";
 import { debounceTime, fromEvent, startWith } from "rxjs";
 
-const LAYOUT_MARGIN = 20;
 const MAX_CONTAINER_WIDTH = 900;
-const DEFAULT_MOBILE_CONTAINER_WIDTH = 335;
 
 let RenderList = ({ data, searchData }) => {
   let [view, setView] = useState("grid");
 
-  let [containerWidth, setContainerWidth] = useState(
-    typeof window !== `undefined` ? Math.min(window.innerWidth - LAYOUT_MARGIN * 2, MAX_CONTAINER_WIDTH) : DEFAULT_MOBILE_CONTAINER_WIDTH
-  );
+  let [containerWidth, setContainerWidth] = useState(MAX_CONTAINER_WIDTH);
   useEffect(() => {
     const subscribtion = fromEvent(window, "resize")
       .pipe(
         debounceTime(250),
-        startWith(DEFAULT_MOBILE_CONTAINER_WIDTH) // need to path some value
+        startWith(MAX_CONTAINER_WIDTH) // need to path some value
       )
       .subscribe(() => setContainerWidth(container.current.clientWidth));
     return () => subscribtion.unsubscribe();
