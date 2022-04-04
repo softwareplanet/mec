4fd21d8b58ddfe11796a608f612ae263f9874f24
Ihor Molchan <ihormo@tahometer.com>
2022-04-04 10:14:56 +0300

    Add useEffect for ios firs rendering

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import { Link } from "gatsby";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import tank from "./tank.svg"
 import addContent from "./add-content.svg"
 import * as styles from "./Header.module.css"
@@ -7,22 +7,24 @@ import arrow from "../../equipment/images/arrow-left.png";
 import clsx from "clsx"
 
 let Header = (props) => {
-    const isIOS = setTimeout(() => {
-        typeof navigator !== 'undefined' ? /iPhone/.test(navigator.userAgent) && !window.MSStream : null
-    }, 0)
+
+    let [isIOS, setIsIOS] = useState(false)
+    useEffect(() => {
+        setIsIOS(typeof navigator !== 'undefined' ? /iPhone/.test(navigator.userAgent) && !window.MSStream : false)
+    }, [])
 
     return (
-    <div className={clsx(styles.container, { [styles.ios]: isIOS })}>
-        <Link to={props.backPath || "/"}>
-            <div className={styles.head}>
-                {props.backPath ? <img height="24px" src={arrow} /> : <img src={tank} alt="" className={styles.logo} />}
-                <h1>{props.name}</h1>
-            </div>
-        </Link>
-        <a href="https://forms.gle/JkwZaui4AjKtvZDe6" target="_blank" rel="noreferrer" >
-            <img src={addContent} width="35px" alt="" />
-        </a>
-    </div>
+        <div className={clsx(styles.container, { [styles.ios]: isIOS })}>
+            <Link to={props.backPath || "/"}>
+                <div className={styles.head}>
+                    {props.backPath ? <img height="24px" src={arrow} /> : <img src={tank} alt="" className={styles.logo} />}
+                    <h1>{props.name}</h1>
+                </div>
+            </Link>
+            <a href="https://forms.gle/JkwZaui4AjKtvZDe6" target="_blank" rel="noreferrer" >
+                <img src={addContent} width="35px" alt="" />
+            </a>
+        </div>
     )
 }
 
