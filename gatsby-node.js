c462d0e4a3a781094ad3a59ab7914c0d656ae316
Ihor Molchan <ihormo@tahometer.com>
2022-04-05 16:48:22 +0300

    Create sourceNodes for version info

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 const { createFilePath } = require(`gatsby-source-filesystem`)
 const path = require('path')
-exports.createPages = async ({graphql, actions}) => {
+exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
   const data = await graphql(
     `
@@ -29,14 +29,14 @@ exports.createPages = async ({graphql, actions}) => {
     createPage({
       path: item.slug,
       component: equipmentPageTemplate,
-      context: {slug: item.slug, imageDir: item.slug + 'images', category: item.frontmatter.category.name}
+      context: { slug: item.slug, imageDir: item.slug + 'images', category: item.frontmatter.category.name }
     });
   }
 }
 
 exports.createSchemaCustomization = ({ actions }) => {
-    const { createTypes } = actions
-    const typeDefs = `
+  const { createTypes } = actions
+  const typeDefs = `
       type Mdx implements Node {
         frontmatter: Frontmatter
       }
@@ -47,5 +47,30 @@ exports.createSchemaCustomization = ({ actions }) => {
         equipment: [Mdx] @link(by: "frontmatter.category.name", from: "name")
       }
     `
-    createTypes(typeDefs)
+  createTypes(typeDefs)
+}
+
+exports.sourceNodes = ({ actions, createNodeId, createContentDigest }) => {
+  const { createNode } = actions;
+
+  const myData = {
+    version: 'v1.0.0-beta1'
+  }
+
+  const nodeContent = JSON.stringify(myData);
+
+  const nodeMeta = {
+    id: createNodeId(`my-data-${myData.key}`),
+    parent: null,
+    children: [],
+    internal: {
+      type: 'versionInfo',
+      mediaType: 'text/html',
+      content: nodeContent,
+      contentDigest: createContentDigest(myData)
+    }
+  }
+
+  const node = Object.assign({}, myData, nodeMeta);
+  createNode(node);
 }
