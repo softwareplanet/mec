8766d1b8406e87466d127c4f542af7efe08aed69
Ruslan Dubyna <ruslandu@tahometer.com>
2022-04-06 15:14:00 +0300

    create sticky header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,31 +1,63 @@
-import { Link } from "gatsby";
-import React, { useEffect, useState } from "react";
-import tank from "./tank.svg"
-import addContent from "./add-content.svg"
-import * as styles from "./Header.module.css"
-import arrow from "../../equipment/images/arrow-left.png";
-import clsx from "clsx"
+import { Link } from 'gatsby';
+import React, { useEffect, useState } from 'react';
+import tank from './tank.svg';
+import addContent from './add-content.svg';
+import * as styles from './Header.module.css';
+import arrow from '../../equipment/images/arrow-left.png';
+import clsx from 'clsx';
 
-let Header = (props) => {
-
-    let [isIOS, setIsIOS] = useState(false)
+let Header = props => {
+    let [isIOS, setIsIOS] = useState(false);
     useEffect(() => {
-        setIsIOS(typeof navigator !== 'undefined' ? /iPhone/.test(navigator.userAgent) && !window.MSStream : false)
-    }, [])
+        setIsIOS(
+            typeof navigator !== 'undefined'
+                ? /iPhone/.test(navigator.userAgent) && !window.MSStream
+                : false
+        );
+    }, []);
+
+    const debounce = func => {
+        let frame;
+        return (...params) => {
+            if (frame) {
+                cancelAnimationFrame(frame);
+            }
+            frame = requestAnimationFrame(() => {
+                func(...params);
+            });
+        };
+    };
+
+    const storeScroll = () => {
+        document.documentElement.dataset.scroll = window.scrollY;
+    };
+    document.addEventListener('scroll', debounce(storeScroll));
+
+    storeScroll();
 
     return (
         <div className={clsx(styles.container, { [styles.ios]: isIOS })}>
-            <Link to={props.backPath || "/"}>
-                <div className={styles.head}>
-                    {props.backPath ? <img height="24px" src={arrow} /> : <img src={tank} alt="" className={styles.logo} />}
-                    <h1>{props.name}</h1>
-                </div>
-            </Link>
-            <a href="https://forms.gle/JkwZaui4AjKtvZDe6" target="_blank" rel="noreferrer" >
-                <img src={addContent} width="35px" alt="" />
-            </a>
+            <div className={styles.content}>
+                <Link to={props.backPath || '/'}>
+                    <div className={styles.head}>
+                        {props.backPath ? (
+                            <img height="24px" src={arrow} />
+                        ) : (
+                            <img src={tank} alt="" className={styles.logo} />
+                        )}
+                        <h1>{props.name}</h1>
+                    </div>
+                </Link>
+                <a
+                    href="https://forms.gle/JkwZaui4AjKtvZDe6"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img src={addContent} width="35px" alt="" />
+                </a>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Header;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import * as styles from './layout.module.css';
+import * as headerStyles from '../Header/Header.module.css';
 import Footer from '../Footer/Footer';
 
 const Layout = props => {
     return (
         <div className={styles.layout}>
+            <Header {...props} />
             <main>
-                <Header {...props} />
                 {props.children}
             </main>
             <Footer />
