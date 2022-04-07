2f6014160c57938866c44b1371228009438cbff0
Serhii Vodianov <serhiivo@tahometer.com>
2022-04-07 17:11:55 +0300

    fix dropdown styles

--- src/components/ToolBar/Dropdown/Dropdown.js
+++ src/components/ToolBar/Dropdown/Dropdown.js
@@ -1,29 +1,41 @@
-import React, { useState } from "react";
-import SearchResults from "../Search/DropdownList.js"
-import * as styles from "../Search/search.module.css";
-import addContent from "../add-content.svg"
-import arrow from "../../../images/hideIcon.png";
+import React, { useState } from 'react';
+import SearchResults from '../Search/DropdownList.js';
+import * as styles from '../Search/dropdown.module.css';
+import addContent from '../add-content.svg';
+import arrow from '../../../images/hideIcon.png';
 
 const Dropdown = ({ data, currEquip }) => {
     const [hidden, setHide] = useState(true);
     return (
         <div className={styles.container}>
-        <div className={styles.container} onClick={() => setHide(!hidden)}>
-            <div className={styles.dropdown}>
+            <div className={styles.dropdown} onClick={() => setHide(!hidden)}>
+                <img
+                    src={arrow}
+                    style={
+                        hidden
+                            ? { transform: 'rotate(0deg)' }
+                            : { transform: 'rotate(180deg)' }
+                    }
+                />
                 <span>{currEquip.frontmatter.title}</span>
-                <img src={arrow} style={hidden ? { transform: "rotate(0deg)" } : { transform: "rotate(180deg)" }} />
             </div>
-            <SearchResults currEquip={currEquip.slug} isHidden={{ display: hidden ? "none" : "block" }} styles={styles} results={
-                data.map((node) => ({
+            <SearchResults
+                currEquip={currEquip.slug}
+                isHidden={{ display: hidden ? 'none' : 'block' }}
+                styles={styles}
+                results={data.map(node => ({
                     slug: node.slug,
-                    title: node.frontmatter.title
-                }))
-            } />
-        </div>
-        <a href="https://forms.gle/JkwZaui4AjKtvZDe6" target="_blank" rel="noreferrer" >
+                    title: node.frontmatter.title,
+                }))}
+            />
+            <a
+                href="https://forms.gle/JkwZaui4AjKtvZDe6"
+                target="_blank"
+                rel="noreferrer"
+            >
                 <img src={addContent} width="35px" alt="" />
-        </a>
+            </a>
         </div>
-    )
-}
+    );
+};
 export default Dropdown;
