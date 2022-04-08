007b108d519641cb6f2d8cf605d7ebd5356dd53a
Ihor Molchan <ihormo@tahometer.com>
2022-04-08 10:11:34 +0300

    Add swipe back event on ios

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,17 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import * as styles from './layout.module.css';
 import Footer from '../Footer/Footer';
+import { useSwipeable } from 'react-swipeable';
 
 const Layout = props => {
     const [height, setHeight] = useState(30);
-    let marginSize = height - 20
+    let marginSize = height - 20;
+
+    const handlers = useSwipeable({
+        onSwipedRight: () => {
+            if (
+                typeof window !== 'undefined' &&
+                window.location.pathname !== '/'
+            ) {
+                window.location.pathname = props.backPath;
+            }
+        },
+    });
 
     return (
-        <div className={styles.layout}>
+        <div className={styles.layout} {...handlers}>
             <Header setHeight={setHeight} {...props} />
-            <main style={{ marginTop: marginSize}}>
-                {props.children}
-            </main>
+            <main style={{ marginTop: marginSize }}>{props.children}</main>
             <Footer />
         </div>
     );
