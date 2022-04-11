2379d72328adf8a3d73ce71c872570edc4c4c86f
Ihor Molchan <ihormo@tahometer.com>
2022-04-11 22:31:33 +0300

    [Refactor]-update styles for add equipment button

--- src/components/ToolBar/Dropdown/Dropdown.js
+++ src/components/ToolBar/Dropdown/Dropdown.js
@@ -7,30 +7,30 @@ import arrow from '../../../images/hideIcon.png';
 const Dropdown = ({ data, currEquip }) => {
     const [hidden, setHide] = useState(true);
     return (
-        <div className={styles.container}>
-            <div className={styles.container} onClick={() => setHide(!hidden)}>
-                <div className={styles.dropdown}>
-                    <span>{currEquip.frontmatter.title}</span>
-                    <img
-                        src={arrow}
-                        style={
-                            hidden
-                                ? { transform: 'rotate(0deg)' }
-                                : { transform: 'rotate(180deg)' }
-                        }
-                    />
-                </div>
-                <SearchResults
-                    currEquip={currEquip.slug}
-                    isHidden={{ display: hidden ? 'none' : 'block' }}
-                    styles={styles}
-                    results={data.map(node => ({
-                        slug: node.slug,
-                        title: node.frontmatter.title,
-                    }))}
+        <div className={styles.container} onClick={() => setHide(!hidden)}>
+            <div className={styles.dropdown}>
+                <span>{currEquip.frontmatter.title}</span>
+                <img
+                    src={arrow}
+                    style={
+                        hidden
+                            ? { transform: 'rotate(0deg)' }
+                            : { transform: 'rotate(180deg)' }
+                    }
                 />
             </div>
-            <AddEquipment />
+            <SearchResults
+                currEquip={currEquip.slug}
+                isHidden={{ display: hidden ? 'none' : 'block' }}
+                styles={styles}
+                results={data.map(node => ({
+                    slug: node.slug,
+                    title: node.frontmatter.title,
+                }))}
+            />
+            <div className={styles.addEquipment}>
+                <AddEquipment />
+            </div>
         </div>
     );
 };
