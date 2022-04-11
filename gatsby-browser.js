c0e58bc353182ce66d50bb16b4ede370ba268641
VitaliyHrabovych <98694678+VitaliyHrabovych@users.noreply.github.com>
2022-04-11 12:30:32 +0300

    Revert "Revert "Merge pull request #42 from softwareplanet/13-progressbar""
    This reverts commit 671e8d964ba6b08739578d3fd198513d93f0a076.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import ViewContext from './src/components/RenderList/Context';
-import { PopUpContext } from './src/components/Pop-up/PopUpContext';
+import { PopUpContext }  from './src/components/Pop-up/PopUpContext';
+import Layout from './src/components/Layout/Layout.js';
+import ProgressContext from './src/components/Layout/ProgressContext';
 
 let Root = ({ children }) => {
     let [view, setView] = useState('grid');
+    let [progressState, setProgressState] = useState(false);
     const [popupState, setPopupState] = useState(false);
-
     return (
-        <ViewContext.Provider value={{ view, setView }}>
-            <PopUpContext.Provider value={{ popupState, setPopupState }}>
-                {children}
-            </PopUpContext.Provider>
-        </ViewContext.Provider>
+        <ProgressContext.Provider value={{ progressState, setProgressState }}>
+            <ViewContext.Provider value={{ view, setView }}>
+                <PopUpContext.Provider value={{ popupState, setPopupState }}>
+                    {children}
+                </PopUpContext.Provider>
+            </ViewContext.Provider>
+        </ProgressContext.Provider>
     );
 };
 
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
-import Header from '../Header/Header';
-import * as styles from './layout.module.css';
-import Footer from '../Footer/Footer';
 import useNotchDetector from '../../customHooks/NotchDetector';
 import useCheckBrowser from '../../customHooks/CheckBrowser';
 import { Helmet } from 'react-helmet';
+import React, { useState, useContext } from 'react';
+import Header from '../Header/Header';
+import * as styles from './layout.module.css';
+import Footer from '../Footer/Footer';
+import { Progressbar } from '../Progressbar/Progressbar';
+import ProgressContext from './ProgressContext';
 
 const Layout = props => {
     const [height, setHeight] = useState(30);
@@ -12,8 +14,13 @@ const Layout = props => {
     const isBrowser = useCheckBrowser(false);
     let marginSize = height - 30;
 
+    const { progressState } = useContext(ProgressContext);
+
     return (
-        <div className={styles.layout}>
+        <div
+            className={styles.layout}
+            style={{ marginBottom: progressState ? 70 : 0 }}
+        >
             <Helmet title="Meqd" defer={false} />
             <Header
                 setHeight={setHeight}
@@ -23,6 +30,7 @@ const Layout = props => {
             />
             <main style={{ marginTop: marginSize }}>{props.children}</main>
             <Footer isIphone={iosWithNotch} isBrowser={isBrowser} />
+            <Progressbar />
         </div>
     );
 };
