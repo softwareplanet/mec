13f2ae6cf56466e909199e2a1cc7fceb2f916861
Ihor Molchan <ihormo@softwareplanet.group>
2022-04-12 16:15:54 +0300

    Delete comments

--- src/components/Slider/SlideElement/SlideElement.js
+++ src/components/Slider/SlideElement/SlideElement.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import * as styles from './SlideElement.module.css';
 import { GatsbyImage } from 'gatsby-plugin-image';
-import { BiImages } from 'react-icons/bi';
 
 export default function SlideElement({ slideImage, allImages, currentImage }) {
-  return (
-    <div className={styles.SlideElement}>
-      <GatsbyImage
-        image={slideImage}
-        // imgStyle={{ maxHeight: '900px' }}
-        // style={{ maxHeight: '70vh' }}
-        alt="Slide image"
-      />
-      {/* <div className={styles.countImages}>
-        <BiImages className={styles.img} />
-        <span>{currentImage} з {allImages}</span>
-      </div> */}
-    </div>
-  )
+    return (
+        <div className={styles.SlideElement}>
+            <GatsbyImage image={slideImage} alt="Slide image" />
+        </div>
+    );
 }
