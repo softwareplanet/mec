39e2a6b3f840a00569ffd8cf1a610b88f5d4a323
Ihor Molchan <ihormo@softwareplanet.group>
2022-04-12 14:41:48 +0300

    Delete package

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,25 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import * as styles from './layout.module.css';
 import Footer from '../Footer/Footer';
-import { useSwipeable } from 'react-swipeable';
 
 const Layout = props => {
     const [height, setHeight] = useState(30);
     let marginSize = height - 20;
 
-    const handlers = useSwipeable({
-        onSwipedRight: () => {
-            if (
-                typeof window !== 'undefined' &&
-                window.location.pathname !== '/'
-            ) {
-                window.location.pathname = props.backPath;
-            }
-        },
-    });
-
     return (
-        <div className={styles.layout} {...handlers}>
+        <div className={styles.layout}>
             <Header setHeight={setHeight} {...props} />
             <main style={{ marginTop: marginSize }}>{props.children}</main>
             <Footer />
