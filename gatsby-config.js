6a07194c239db4c958b97a00353fdf5aec0120ee
Ihor Molchan <ihormo@softwareplanet.group>
2022-04-21 14:20:33 +0300

    Update gatsby-config for new App Name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,111 +1,117 @@
 const { resolve } = require('path');
 
 module.exports = {
-  siteMetadata: {
-    title: `MEQD`,
-    siteUrl: `https://stop-orda.netlify.app`
-  },
-  plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-image",
-    "gatsby-source-local-git-modern",
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        remarkPlugins: [
-          [require('remark-oembed'), { syncWidget: true }]
-        ]
-      },
+    siteMetadata: {
+        title: `Meqd`,
+        siteUrl: `https://stop-orda.netlify.app`,
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "pages",
-        "path": "./src/pages/"
-      },
-      __key: "pages"
-    },
-    `gatsby-transformer-yaml`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `./src/equipment/`,
-        name: 'equipments'
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `MEQD`,
-        short_name: `MEQD`,
-        start_url: `/`,
-        background_color: `#F5F5F5`,
-        theme_color: `#F5F5F5`,
-        display: `standalone`,
-        icon: `src/images/icon.png`,
-        cache_busting_mode: 'none',
-        crossOrigin: `use-credentials`,
-        icons: [
-          {
-            src: "public/icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-          {
-            src: "public/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          },
-          {
-            src: "public/icons/icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable"
-          }
-        ]
-      },
-    },
-    {
-      resolve: `local-offline-plugin`,
-      options: {
-        precachePages: ["/*", "/**/**/index.html"],
-        workboxConfig: {
-          globPatterns: [
-            'offline-plugin-app-shell-fallback/index.html',
-            'flexsearch_index.json',
-            'public/icons/*', "favicon*.png", "**/*.webp"
-          ]
-        }
-      }
-    },
-    {
-      resolve: 'local-search-plugin',
-      options: {
-        languages: ['uk'],
-        type: 'Mdx',
-        fields: [
-          {
-            name: 'title',
-            indexed: true,
-            attributes: {
-              encode: false
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-image',
+        'gatsby-source-local-git-modern',
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.mdx`, `.md`],
+                remarkPlugins: [
+                    [require('remark-oembed'), { syncWidget: true }],
+                ],
             },
-            resolver: 'frontmatter.title',
-            store: true
-          },
-          {
-            name: 'slug',
-            indexed: false,
-            resolver: node => node.fileAbsolutePath.split("/").slice(-3, -1).join("/") + "/",
-            store: true,
-          }
-        ]
-      }
-    }
-  ]
-}
+        },
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'pages',
+                path: './src/pages/',
+            },
+            __key: 'pages',
+        },
+        `gatsby-transformer-yaml`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `./src/equipment/`,
+                name: 'equipments',
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Meqd`,
+                short_name: `Meqd`,
+                start_url: `/`,
+                background_color: `#F5F5F5`,
+                theme_color: `#F5F5F5`,
+                display: `standalone`,
+                icon: `src/images/icon.png`,
+                cache_busting_mode: 'none',
+                crossOrigin: `use-credentials`,
+                icons: [
+                    {
+                        src: 'public/icons/icon-144x144.png',
+                        sizes: '144x144',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: 'public/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                    {
+                        src: 'public/icons/icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `local-offline-plugin`,
+            options: {
+                precachePages: ['/*', '/**/**/index.html'],
+                workboxConfig: {
+                    globPatterns: [
+                        'offline-plugin-app-shell-fallback/index.html',
+                        'flexsearch_index.json',
+                        'public/icons/*',
+                        'favicon*.png',
+                        '**/*.webp',
+                    ],
+                },
+            },
+        },
+        {
+            resolve: 'local-search-plugin',
+            options: {
+                languages: ['uk'],
+                type: 'Mdx',
+                fields: [
+                    {
+                        name: 'title',
+                        indexed: true,
+                        attributes: {
+                            encode: false,
+                        },
+                        resolver: 'frontmatter.title',
+                        store: true,
+                    },
+                    {
+                        name: 'slug',
+                        indexed: false,
+                        resolver: node =>
+                            node.fileAbsolutePath
+                                .split('/')
+                                .slice(-3, -1)
+                                .join('/') + '/',
+                        store: true,
+                    },
+                ],
+            },
+        },
+    ],
+};
