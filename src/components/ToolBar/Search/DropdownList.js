db5f974e98cd80757ccd6ce4325f9fed5f768c8a
Ihor Molchan <ihormo@softwareplanet.group>
2022-05-03 18:09:06 +0300

    Use sort list function in components

--- src/components/ToolBar/Search/DropdownList.js
+++ src/components/ToolBar/Search/DropdownList.js
@@ -1,19 +1,26 @@
-import React from "react";
-import { Link } from "gatsby";
-import clsx from "clsx";
+import React from 'react';
+import { Link } from 'gatsby';
+import clsx from 'clsx';
+import SortList from '../../../customFunctions/SortList';
 
 const DropdownList = ({ styles, results, isHidden, currEquip }) => {
+    const sortResult = SortList(results);
 
     return (
         <ul className={styles.resultUl} id="resultsList" style={isHidden}>
-            {results.map((page, i) => (
+            {sortResult.map((page, i) => (
                 <Link to={`/${page.slug}`} key={i}>
-                    <li className={clsx(styles.item, { [styles.active]: currEquip === page.slug })} key={i}>
+                    <li
+                        className={clsx(styles.item, {
+                            [styles.active]: currEquip === page.slug,
+                        })}
+                        key={i}
+                    >
                         {page.title}
                     </li>
                 </Link>
             ))}
         </ul>
-    )
-}
+    );
+};
 export default DropdownList;
