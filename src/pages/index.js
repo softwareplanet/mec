7658b4773fe919f3452676eacf5b2c5a726b4512
Ihor Molchan <ihormo@softwareplanet.group>
2022-05-04 17:50:38 +0300

    Set landing page first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,28 @@
-import "../components/reset.css";
-import React from "react";
-import RenderList from "../components/RenderList/RenderList";
-import { graphql } from "gatsby";
-import Layout from "../components/Layout/Layout";
+import '../components/reset.css';
+import React from 'react';
+import RenderList from '../components/RenderList/RenderList';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout/Layout';
+import LandingPage from '../components/Landing/LandingPage';
 
 export const query = graphql`
-  query {
-    allCategoriesYaml {
-      nodes {
-        name
-        title
-        image {
-          childImageSharp {
-            gatsbyImageData(width: 240, height: 240)
-          }
+    query {
+        allCategoriesYaml {
+            nodes {
+                name
+                title
+                image {
+                    childImageSharp {
+                        gatsbyImageData(width: 240, height: 240)
+                    }
+                }
+            }
         }
-      }
     }
-   }
 `;
 
 let FirstPage = ({ data }) => {
-  return (
-    <Layout name="Військова техніка">
-      <RenderList
-        data={data.allCategoriesYaml.nodes.map((n) => ({
-          path: n.name,
-          image: n.image.childImageSharp,
-          title: n.title,
-        }))}
-      />
-    </Layout>
-  );
+    return <LandingPage />;
 };
 
 export default FirstPage;
