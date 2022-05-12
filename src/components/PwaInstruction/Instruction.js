cc94ffc42e8ae450e18e1d3f2ad7bdde1f762a60
Ihor Molchan <ihormo@softwareplanet.group>
2022-05-12 13:00:16 +0300

    Update markup and styles for pwa instructions

--- src/components/PwaInstruction/Instruction.js
+++ src/components/PwaInstruction/Instruction.js
@@ -1,23 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as styles from './Instruction.module.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import noScroll from 'no-scroll';
 import Slider from 'react-slick';
 import SlideArrow from '../Slider/SliderComponent/SlideArrow/SlideArrow';
+import firstSlide from './slideImages/slide(1).png';
+import secondSlide from './slideImages/slide(2).png';
+import thirdSlide from './slideImages/slide(3).png';
 
 export default function Instruction() {
+    const [currentSlide, setCurrentSlide] = useState(0);
+
     useEffect(() => {
         noScroll.on();
     });
 
+    const handleAfterChange = index => {
+        setCurrentSlide(index);
+    };
+
     const settings = {
-        speed: 100,
+        speed: 0,
         infinite: false,
         slidesToShow: 1,
         slidesToScroll: 1,
-        nextArrow: <SlideArrow type="next" isModal={false} />,
-        prevArrow: <SlideArrow isModal={false} />,
+        swipe: false,
+        afterChange: handleAfterChange,
+        nextArrow: <SlideArrow type="next" isModal={true} />,
+        prevArrow: <SlideArrow isModal={true} />,
     };
 
     return (
@@ -25,18 +36,30 @@ export default function Instruction() {
             <div className={styles.container}>
                 <Slider {...settings}>
                     <div className={styles.slideElement}>
-                        <h3>
-                            Щоб додати застосунок на початковий екран, потрібно
-                            натиснути на іконку “Поділіться”{' '}
-                        </h3>
+                        <img
+                            src={firstSlide}
+                            alt=""
+                            style={{ maxWidth: '100vw' }}
+                        />
                     </div>
                     <div className={styles.slideElement}>
-                        <h3>Some text</h3>
+                        <img
+                            src={secondSlide}
+                            alt=""
+                            style={{ maxWidth: '100vw' }}
+                        />
                     </div>
                     <div className={styles.slideElement}>
-                        <h3>Some text</h3>
+                        <img
+                            src={thirdSlide}
+                            alt=""
+                            style={{ maxWidth: '100vw' }}
+                        />
                     </div>
                 </Slider>
+            </div>
+            <div className={styles.counter}>
+                <span>{currentSlide + 1} </span>/ 3
             </div>
         </div>
     );
